09c49b4aa978e75b86cfb50da15265c6fd28c7cd
TeppalaSateesh <teppalasateesh@gmail.com>
2022-03-22 18:08:31 +0530

    api url changed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MovieCard from "./MovieCard";
 import './App.css';
 import SearchIcon from './search.svg';
 
-const API_URL = 'http://www.omdbapi.com?apikey=3db0d849';
+const API_URL = 'https://www.omdbapi.com?apikey=3db0d849';
 
 const App = () => {
 
